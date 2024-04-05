f9099249b2dec3d734e2a47781950c64eb2759c5
O J <1702825+Olanrewaju@users.noreply.github.com>
2024-04-05 18:39:19 +0400

    Update: O J

--- js/js-mobile-menu-close.js
+++ js/js-mobile-menu-close.js
@@ -1,14 +1,12 @@
-<script>
-	function mobileMenuClose() {
-		//Switching menu buttons to close state
-			document.getElementById("topnav-menu-button-img").src = "');bloginfo('stylesheet_directory'); echo('/img/header/topnav-menu-button.png";
-			$(".ovl-m").css("display", "none");
-			document.getElementById("logo-mobile-image").src = "');bloginfo('stylesheet_directory'); echo('/img/header/logo-none.png";
-			document.getElementById("search-submit-mobile").src = "');bloginfo('stylesheet_directory'); echo('/img/header/search-icon.png";
-			//Toggling the mobile menu
-			$(".tmm-m").animate({right: "-100vh"}, 800);
-			$(".tnl").css("display", "block");
-			//Enable page scrolling: resetting the class for the body
-			$(".mb").css("overflow", "unset");
-	}
-</script>
+function mobileMenuClose() {
+	//Switching menu buttons to close state
+		document.getElementById("topnav-menu-button-img").src = "');bloginfo('stylesheet_directory'); echo('/img/header/topnav-menu-button.png";
+		$(".ovl-m").css("display", "none");
+		document.getElementById("logo-mobile-image").src = "');bloginfo('stylesheet_directory'); echo('/img/header/logo-none.png";
+		document.getElementById("search-submit-mobile").src = "');bloginfo('stylesheet_directory'); echo('/img/header/search-icon.png";
+		//Toggling the mobile menu
+		$(".tmm-m").animate({right: "-100vh"}, 800);
+		$(".tnl").css("display", "block");
+		//Enable page scrolling: resetting the class for the body
+		$(".mb").css("overflow", "unset");
+}
--- js/js-outside-popup-click-mobile-menu.js
+++ js/js-outside-popup-click-mobile-menu.js
@@ -1,16 +1,14 @@
-<script>
-	//Outside popup click - close the mobile menu
-	$(document).mouseup(function (e) {
-		var container2 = $(".tmm");
-		if(!container2.is(e.target) && container2.has(e.target).length === 0){
-			if ($(".tmm").css("right") === "0px"){
-				document.getElementById("topnav-menu-button-img").src = "');bloginfo('stylesheet_directory'); echo('/img/header/topnav-menu-button.png";
-				transition.begin(overlay, "background-color rgba(32,33,37,0.5) #35434a 4000ms ease-out");
-				$(".ovl-m").css("display", "none");
-				$(".mb").css("overflow", "unset");
-				//Toggling the mobile menu
-				container2.animate({right: "-100vh"}, 800);
-			}	
-		}				
-	});	
-</script>
+//Outside popup click - close the mobile menu
+$(document).mouseup(function (e) {
+	var container2 = $(".tmm");
+	if(!container2.is(e.target) && container2.has(e.target).length === 0){
+		if ($(".tmm").css("right") === "0px"){
+			document.getElementById("topnav-menu-button-img").src = "');bloginfo('stylesheet_directory'); echo('/img/header/topnav-menu-button.png";
+			transition.begin(overlay, "background-color rgba(32,33,37,0.5) #35434a 4000ms ease-out");
+			$(".ovl-m").css("display", "none");
+			$(".mb").css("overflow", "unset");
+			//Toggling the mobile menu
+			container2.animate({right: "-100vh"}, 800);
+		}	
+	}				
+});	
